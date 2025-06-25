576aa62e048314f039145e72e66436e9c137f89d
nguyenhuuloc18050078 <18050078@student.bdu.edu.vn>
2025-06-25 08:08:39 +0700

    update config mongodb

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 # Rename example.env to .env
-cp example.env .env
-cp nodejs-app/example.env nodejs-app/.env
-cp dashboard/example.env dashboard/.env
+# cp example.env .env
+# cp nodejs-app/example.env nodejs-app/.env
+# cp dashboard/example.env dashboard/.env
 
 # Create nodejs-data
 mkdir nodejs-data
+mkdir nodejs-data/mongodb
+
 sudo chown -R 999:999 ./nodejs-data
+sudo chown -R 999:999 ./nodejs-data/mongodb
 sudo chmod -R 755 ./nodejs-data
-chmod 777 nodejs-data
+sudo chmod -R 755 ./nodejs-data/mongodb
+chmod 777 ./nodejs-data
+chmod 777 ./nodejs-data/mongodb
 
 # Show status
 echo "Setup complete!"
